9acd6c9119d0c1c6005dc4c28ed27c185e56e4df
darkkeks <darkkeks@rambler.ru>
2017-11-13 23:28:22 +0300

    First day evening, dont want to code any more

--- src/main/java/MyMove.java
+++ src/main/java/MyMove.java
@@ -1,6 +1,55 @@
+import model.Move;
+import model.VehicleType;
+
+import java.util.function.Predicate;
 
 public class MyMove {
 
-    public Mov
+    public Move move = new Move();
+
+    public void clearAndSelect(int left, int top, int right, int bottom, VehicleType type) {
+    }
+
+    public void clearAndSelect(VehicleType type) {
+    }
+
+    public void clearAndSelect(int groupId) {
+    }
+
+    public void addToSelection(int left, int top, int right, int bottom, VehicleType type) {
+    }
+
+    public void addToSelection(VehicleType type) {
+    }
+
+    public void addToSelection(int groupId) {
+    }
+
+    public void deselect() {
+    }
+
+    public void assign(int groupId) {
+    }
+
+    public void disband(int groupId){
+    }
+
+    public void move(double x, double y, double maxSpeed) {
+    }
+
+    public void move(double x, double y) {
+    }
+
+    public void rotate(double x, double y, double angle, double maxSpeed, double maxAngularSpeed) {
+    }
+
+    public void rotate(double x, double y, double angle) {
+    }
+
+    public void scale(double x, double y, double factor) {
+    }
+
+    public void nuke(double x, double y, long vehId) {
+    }
 
 }
